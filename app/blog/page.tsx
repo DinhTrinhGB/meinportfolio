d10421e32504b8da03e5ac57f7a65f333bcec370
DinhTrinhGB <109355639+DinhTrinhGB@users.noreply.github.com>
2025-04-08 00:17:57 +0200

    Add files via upload

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -43,7 +43,3 @@ export default function BlogPosts() {
     </section>
   );
 }
-      </div>
-    </section>
-  );
-}
--- app/page.tsx
+++ app/page.tsx
@@ -16,31 +16,48 @@ export default function Page() {
         />
       </a>
       <h1 className="mb-8 text-2xl font-medium">
-        Portfolio
+        Portfolio template!
       </h1>
       <div className="prose prose-neutral dark:prose-invert">
         <p>
-          Ich bin Dinh Trinh Gia Bao und bin ein Student an der Technischen Universität München.
+          A clean, fast, and lightweight portfolio template built with Next.js,
+          Vercel, and Tailwind CSS.
         </p>
         <p>
-          Das Template das hier verwendet wird habe ich nicht selbst geschrieben, sondern von {""}
+          Nextfolio has everything you need for a portfolio: MDX blog, SEO, RSS,
+          Atom & JSON feeds, analytics, Tweet & YouTube embeds, KaTeX and {""}
           <a
             target="_blank"
-            href="https://github.com/1msirius/Nextfolio"
+            href="https://github.com/1msirius/Nextfolio?tab=readme-ov-file#features"
           >
-            1msirius
+            more
           </a>
           .
         </p>
         <p>
-          
-          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
-            nicht auf diesen Link drücken
-          </a>
+          Nextfolio is{" "}
+          <a href={socialLinks.github} target="_blank">
+            open-source
+          </a>{" "}
+          and fully customizable, making it easy to add more features.
         </p>
-        
         <p>
-          Ich weiß nicht was ich hier schreiben soll also ist die Seite größtenteils Leer.
+          <a
+            href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2F1msirius%2FNextfolio"
+            target="_blank"
+          >
+            Deploy
+          </a>{" "}
+          your Nextfolio site with Vercel in minutes and follow the set up
+          instructions in the{" "}
+          <a href="/blog/getting-started">Getting Started</a> post.
+        </p>
+        <p>
+          Built and maintained by{" "}
+          <a href="https://imsirius.xyz/" target="_blank">
+            Sirius
+          </a>
+          .
         </p>
       </div>
     </section>
