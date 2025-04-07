56f2cb4efb70fda3af3c2569c14a4fd5896a0d30
DinhTrinhGB <109355639+DinhTrinhGB@users.noreply.github.com>
2025-04-08 00:27:16 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,50 +16,32 @@ export default function Page() {
         />
       </a>
       <h1 className="mb-8 text-2xl font-medium">
-        Portfolio template!
+        Portfolio
       </h1>
       <div className="prose prose-neutral dark:prose-invert">
         <p>
-          A clean, fast, and lightweight portfolio template built with Next.js,
-          Vercel, and Tailwind CSS.
+          Ich bin Dinh Trinh Gia Bao und bin ein Student an der Technischen Universität München.
         </p>
         <p>
-          Nextfolio has everything you need for a portfolio: MDX blog, SEO, RSS,
-          Atom & JSON feeds, analytics, Tweet & YouTube embeds, KaTeX and {""}
+          Das Template das hier verwendet wird habe ich nicht selbst geschrieben, sondern von {""}
           <a
             target="_blank"
-            href="https://github.com/1msirius/Nextfolio?tab=readme-ov-file#features"
+            href="https://github.com/1msirius/Nextfolio"
           >
-            more
+            1msirius
           </a>
           .
         </p>
         <p>
-          Nextfolio is{" "}
-          <a href={socialLinks.github} target="_blank">
-            open-source
-          </a>{" "}
-          and fully customizable, making it easy to add more features.
-        </p>
-        <p>
-          <a
-            href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2F1msirius%2FNextfolio"
-            target="_blank"
-          >
-            Deploy
-          </a>{" "}
-          your Nextfolio site with Vercel in minutes and follow the set up
-          instructions in the{" "}
-          <a href="/blog/getting-started">Getting Started</a> post.
-        </p>
-        <p>
-          Built and maintained by{" "}
-          <a href="https://imsirius.xyz/" target="_blank">
-            Sirius
+          
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
+            nicht auf diesen Link drücken
           </a>
-          .
+        </p>
+        
+        <p>
+          Ich weiß nicht was ich hier schreiben soll also ist die Seite größtenteils Leer.
         </p>
       </div>
     </section>
   );
-}
