c3edf92c122b6d3242bcd2e56932485e9357d812
DinhTrinhGB <109355639+DinhTrinhGB@users.noreply.github.com>
2025-04-07 23:48:25 +0200

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { projects } from "./project-data";
 
 export const metadata: Metadata = {
-  title: "projects",
+  title: "Projects",
   description: "Nextfolio Projects",
 };
 
