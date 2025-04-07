567c670a8e25b1f34c241c1a92725422a8ae43b8
DinhTrinhGB <109355639+DinhTrinhGB@users.noreply.github.com>
2025-04-08 00:22:26 +0200

    Add files via upload

---
+++ config.ts
@@ -0,0 +1,16 @@
+export const metaData = {
+  baseUrl: "https://meinportfolio-seven.vercel.app/",
+  title: "Portfolio",
+  name: "Dinh Trinh Gia Bao",
+  ogImage: "/opengraph-image.png",
+  description:
+    "Website",
+};
+
+export const socialLinks = {
+  twitter: "https://x.com",
+  github: "https://github.com/DinhTrinhGB",
+  instagram: "https://www.instagram.com/",
+  linkedin: "https://www.linkedin.com/",
+  email: "mailto:bdinhtrinh@gmail.com",
+};
